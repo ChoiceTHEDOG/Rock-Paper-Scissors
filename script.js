d3d5dd3a0048881c9d6e5ba70b2c81caaf2657c9
Valentin <valen.manestar@gmail.com>
2022-02-22 20:33:02 -0300

    Guardando por las dudas

---
+++ script.js
@@ -0,0 +1,73 @@
+
+const container = document.querySelector('#container');
+
+const rockBtn = document.createElement("button");
+rockBtn.classList.add('content');
+rockBtn.textContent = 'Rock';
+
+const paperBtn = document.createElement("button");
+paperBtn.classList.add('content');
+paperBtn.textContent = 'Paper';
+
+const scissorsBtn = document.createElement("button");
+scissorsBtn.classList.add('content');
+scissorsBtn.textContent = 'Scissors';
+
+container.appendChild(rockBtn);
+container.appendChild(paperBtn);
+container.appendChild(scissorsBtn);
+
+
+
+// Funcion que hace la jugada de la computadora 
+
+const myArray = ["Rock", "Paper", "Scissors"];
+
+function computerPlay() {
+  return myArray[~~(Math.random() * myArray.length)];
+}   
+  
+console.log(computerPlay())
+    
+          // Round Algorithm 
+  
+let playerScore = 0;
+let computerScore = 0;
+
+  function playRound(playerSelection, computerSelection) {
+  
+      if(playerSelection.toUpperCase() === computerSelection.toUpperCase()) {    // Chequamos si son lo mismo, en caso de que si lo damos como empate
+        playerScore += 0
+        computerScore += 0
+        
+        return "Its a Draw"
+  
+      } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "SCISSORS") {  // Win round
+        playerScore += 1
+        return "It's a Win!!! Your Rock wins over the computer Scissors." 
+  
+      } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "PAPER") {    // Loost round
+        computerScore += 1
+        return "It's a lost round :( Your Rock looses over the computer Paper."
+   
+      }else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "ROCK") {      // Win round
+        playerScore += 1
+        return "It's a Win!!! Your Paper wins over the computer rock."
+  
+      }else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "SCISSORS") {    // Loost round
+        computerScore += 1 
+        return "It's a lost round :( Your Paper looses over the computer Scissors."
+   
+      }else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "ROCK") {   // Loost round
+        computerScore += 1
+        return "It's a lost round :( Your Scissors looses over the computer Rock."
+  
+      }else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "PAPER") {   // Win round
+        playerScore += 1
+        return "It's a Win!!! Your scissors wins over the computer paper."
+   
+      } else {
+          alert("Paso Algo")
+      }
+  }
+  
