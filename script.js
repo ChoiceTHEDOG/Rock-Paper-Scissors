ccb0628ea86defdf45228ee8d293c7599d4a2faf
Valentin <valen.manestar@gmail.com>
2022-03-02 20:49:46 -0300

    los resultados en vivo andan, falta declarar al ganador

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 
-let playerScore = 0;
-let computerScore = 0;
-
 // botones de elecion del jugador 
 
 const container = document.querySelector('#container');
@@ -44,31 +41,6 @@ rockBtn.addEventListener('click', () => {
   });  
 
 
- // div de los resultados 
-
- const winerDiv = document.createElement("div");
- winerDiv.textContent = "The game result is:";
-
- const tempDiv = document.createElement("div");
- tempDiv.setAttribute("id", "results")
-
- const resultDiv = document.createElement("div");
-  resultDiv.textContent = "The points are:";
- 
- const playerPoints = document.createElement("p");
- playerPoints.textContent = "Player points:" + " " + playerScore;
- 
- const computerPoints = document.createElement("p");
- computerPoints.textContent = "Computer points" + " " + computerScore;
- 
- container.appendChild(winerDiv);
- container.appendChild(tempDiv);
- container.appendChild(resultDiv);
- resultDiv.appendChild(playerPoints);
- resultDiv.appendChild(computerPoints);
-
-
-
 // Funcion que hace la jugada de la computadora 
 
 const myArray = ["Rock", "Paper", "Scissors"];
@@ -79,46 +51,84 @@ function computerPlay() {
     
           // Round Algorithm 
   
-
-
+  let playerScore = 0;
+  
+  let computerScore = 0;
+  
   function playRound(playerSelection, computerSelection) {
   
       if(playerSelection.toUpperCase() === computerSelection.toUpperCase()) {    // Chequamos si son lo mismo, en caso de que si lo damos como empate
-        playerScore += 0
-        computerScore += 0
         
         return "Its a Draw"
   
       } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "SCISSORS") {  // Win round
-        playerScore += 1
+        playerPoints += 1;
+        updateScore(playerPoints, 'player'); 
         return "It's a Win!!! Your Rock wins over the computer Scissors." 
   
       } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "PAPER") {    // Loost round
-        computerScore += 1
+        cpuPoints += 1;
+        updateScore(cpuPoints, 'cpu');
         return "It's a lost round :( Your Rock looses over the computer Paper."
    
       }else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "ROCK") {      // Win round
-        playerScore += 1
+        playerPoints += 1;
+        updateScore(playerPoints, 'player');
         return "It's a Win!!! Your Paper wins over the computer rock."
   
       }else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "SCISSORS") {    // Loost round
-        computerScore += 1 
+        cpuPoints += 1;
+        updateScore(cpuPoints, 'cpu');
         return "It's a lost round :( Your Paper looses over the computer Scissors."
    
       }else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "ROCK") {   // Loost round
-        computerScore += 1
+        cpuPoints += 1;
+        updateScore(cpuPoints, 'cpu');
         return "It's a lost round :( Your Scissors looses over the computer Rock."
   
       }else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "PAPER") {   // Win round
-        playerScore += 1
+        playerPoints += 1;
+        updateScore(playerPoints, 'player');
         return "It's a Win!!! Your scissors wins over the computer paper."
    
       } else {
           alert("Paso Algo")
       }
-      playerPoints.textContent = playerScore; 
-      computerPoints.textContent = computerScore;
   }
 
-resultDiv.appendChild(playerPoints);
-resultDiv.appendChild(computerPoints);
+  function announceWinner()  {
+    if (playerPoints == 5) {
+      alert("The Player Wins!!!")
+    }
+    else if(cpuPoints == 5) {
+      alert("The Computer Wins!!!");
+    }
+  };  
+
+  const updateScore = (score, playerName) => {
+    document.querySelector(`.${playerName}-score .score`).textContent = score;
+  };
+  
+   // div de los resultados 
+
+ const winerDiv = document.createElement("div");
+ winerDiv.textContent = "The game result is:";
+
+ const tempDiv = document.createElement("div");
+ tempDiv.setAttribute("id", "results")
+
+ const resultDiv = document.createElement("div");
+  resultDiv.textContent = "The points are:";
+ 
+ 
+ 
+ container.appendChild(winerDiv);
+ container.appendChild(tempDiv);
+ container.appendChild(resultDiv);
+
+ let playerPoints = parseInt(0);
+ let cpuPoints = parseInt(0);
+ 
+
+updateScore(0, 'player');
+updateScore(0, 'cpu');
